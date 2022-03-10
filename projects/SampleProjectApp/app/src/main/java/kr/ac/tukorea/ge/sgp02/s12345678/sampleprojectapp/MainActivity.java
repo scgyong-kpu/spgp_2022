6748869b2143f041f9f8ec060a388ddc6e939c51
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-10 17:15:28 +0900

    added button

--- projects/SampleProjectApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sampleprojectapp/MainActivity.java
+++ projects/SampleProjectApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sampleprojectapp/MainActivity.java
@@ -1,6 +1,9 @@
 package kr.ac.tukorea.ge.sgp02.s12345678.sampleprojectapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.nameText);
+        tv.setText("Program started");
+
+        Button btn = findViewById(R.id.btnPushMe);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("Button Pushed");
+            }
+        });
     }
 }
