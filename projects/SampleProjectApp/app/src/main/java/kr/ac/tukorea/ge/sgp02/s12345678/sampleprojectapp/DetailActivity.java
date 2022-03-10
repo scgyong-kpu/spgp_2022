57a3daa86475681ffec1dc388d6ccc355cfe00f6
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-10 16:33:25 +0900

    Added DetailActivity

--- projects/SampleProjectApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sampleprojectapp/DetailActivity.java
+++ projects/SampleProjectApp/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sampleprojectapp/DetailActivity.java
@@ -1,14 +1,14 @@
 package kr.ac.tukorea.ge.sgp02.s12345678.sampleprojectapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
     }
 }
