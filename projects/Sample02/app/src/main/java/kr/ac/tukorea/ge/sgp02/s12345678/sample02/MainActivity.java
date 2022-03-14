41067fd8529f89101f75e686ac15cf7f3b4de705
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-14 16:58:41 +0900

    added findViewById, setText, setOnClickListener

--- projects/Sample02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sample02/MainActivity.java
+++ projects/Sample02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sample02/MainActivity.java
@@ -3,6 +3,9 @@ package kr.ac.tukorea.ge.sgp02.s12345678.sample02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.subText);
+        tv.setText("I am a good programmer");
+
+        Button btn = findViewById(R.id.btnPushMe);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("Clicked !!!");
+            }
+        });
     }
 
 }
