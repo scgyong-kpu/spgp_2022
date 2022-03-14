e3e5aaf01501b21f863fc3dd2befb0241497efc7
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-14 17:15:35 +0900

    Local var => Member var

--- projects/Sample02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sample02/MainActivity.java
+++ projects/Sample02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sample02/MainActivity.java
@@ -4,23 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView subTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.subText);
-        tv.setText("I am a good programmer");
+        subTextView = findViewById(R.id.subText);
+        subTextView.setText("I am a good programmer");
 
     }
 
     public void onBtnPushMe(View view) {
-        TextView tv = findViewById(R.id.subText);
-        tv.setText("Clicked !!!");
+        //TextView tv = findViewById(R.id.subText);
+        subTextView.setText("Clicked !!!");
     }
 }
