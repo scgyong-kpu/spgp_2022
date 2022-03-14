e75866592ca57c836482be9ff8c58e5a12e59c7a
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-14 16:06:06 +0900

    Sample02 skeleton & Added DetailActivity

---
+++ projects/Sample02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/sample02/DetailActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.ge.sgp02.s12345678.sample02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
