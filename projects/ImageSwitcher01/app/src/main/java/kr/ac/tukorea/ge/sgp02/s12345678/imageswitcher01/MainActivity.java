3b4de784d567405851327359e20eada47ace3440
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-16 09:42:33 +0900

    ImageSwitcher Skeleton

---
+++ projects/ImageSwitcher01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher01/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.ge.sgp02.s12345678.imageswitcher01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
