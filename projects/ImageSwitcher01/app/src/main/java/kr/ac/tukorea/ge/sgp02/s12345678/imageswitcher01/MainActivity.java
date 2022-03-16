2639c06d85479d5802c97526be76891144eef0d0
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-16 10:24:52 +0900

    added prev/next button

--- projects/ImageSwitcher01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher01/MainActivity.java
+++ projects/ImageSwitcher01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher01/MainActivity.java
@@ -3,12 +3,23 @@ package kr.ac.tukorea.ge.sgp02.s12345678.imageswitcher01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnPrev(View view) {
+        Log.d(TAG, "Prev Button Pressed");
+    }
+    public void onBtnNext(View view) {
+        Log.d(TAG, "Next Button Pressed");
     }
 }
