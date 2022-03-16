eaa89c5b60c0f84a78602a82816539795aa20e95
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-16 12:11:12 +0900

    added Log on onBtn* methods

--- projects/ImageSwitcher02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher02/MainActivity.java
+++ projects/ImageSwitcher02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher02/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.tukorea.ge.sgp02.s12345678.imageswitcher02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnPrev(View view) {
+        Log.d("MainActivity", "Prev pressed");
+    }
+    public void onBtnNext(View view) {
+        Log.d("MainActivity", "Next pressed");
     }
 }
