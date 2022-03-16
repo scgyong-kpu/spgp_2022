605e37bfe4df03e3820c6584ed0b6706d772ac16
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-16 12:33:35 +0900

    page 변경 화면에 반영.

--- projects/ImageSwitcher02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher02/MainActivity.java
+++ projects/ImageSwitcher02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/imageswitcher02/MainActivity.java
@@ -5,21 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private int pageNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setPage(1);
     }
 
     public void onBtnPrev(View view) {
         Log.d(TAG, "Prev pressed");
+        setPage(pageNumber - 1);
     }
     public void onBtnNext(View view) {
         Log.d(TAG, "Next pressed");
+        setPage(pageNumber + 1);
+    }
+
+    private void setPage(int page) {
+        if (page < 1 || page > 5) {
+            return;
+        }
+        pageNumber = page;
+        TextView pageTextView = findViewById(R.id.pageTextView);
+        String text = pageNumber + " / " + 5;
+        pageTextView.setText(text);
     }
 }
