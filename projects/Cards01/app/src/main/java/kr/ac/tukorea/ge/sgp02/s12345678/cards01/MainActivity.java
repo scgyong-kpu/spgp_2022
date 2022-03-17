14fe052b8509b2ba545b74445cea0497da52f1ab
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-17 16:19:48 +0900

    card index 를 얻어 출력.

--- projects/Cards01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/cards01/MainActivity.java
+++ projects/Cards01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/cards01/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int[] BUTTON_IDS = new int[] {
+            R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
+            R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
+            R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
+            R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Restart");
     }
     public void onBtnCard(View view) {
-        Log.d(TAG, "Card: " + view.getId());
+        int cardIndex = findButtonIndex(view.getId());
+        Log.d(TAG, "Card Index: " + cardIndex);
+    }
+
+    private int findButtonIndex(int id) {
+        for (int i = 0; i < BUTTON_IDS.length; i++) {
+            if (id == BUTTON_IDS[i]) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
