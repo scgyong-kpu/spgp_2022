0ef9c2cafb578b7abafdfc6e85cc0671d9ff3a4b
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-21 16:23:21 +0900

    card id 대신 index 출력

--- projects/Cards02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/cards02/MainActivity.java
+++ projects/Cards02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/cards02/MainActivity.java
@@ -9,6 +9,12 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int[] BUTTON_IDS = new int[] {
+            R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
+            R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
+            R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
+            R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onBtnRestart");
     }
     public void onBtnCard(View view) {
-        Log.d(TAG, "onBtnCard: " + view.getId());
+        int btnIndex = findButtonIndex(view.getId());
+        Log.d(TAG, "onBtnCard: " + btnIndex);
+    }
+
+    private int findButtonIndex(int id) {
+        for (int i = 0; i < BUTTON_IDS.length; i++) {
+            if (id == BUTTON_IDS[i]) {
+                return i;
+            }
+        }
+        Log.e(TAG, "Cannot find the button with id: " + id);
+        return -1;
     }
 }
