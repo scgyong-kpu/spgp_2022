a41b2dce45721ec24bb4b33b55fda3c8d7feb733
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-21 16:14:10 +0900

    카드 누르면 Card ID 를 출력

--- projects/Cards02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/cards02/MainActivity.java
+++ projects/Cards02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/cards02/MainActivity.java
@@ -3,12 +3,23 @@ package kr.ac.tukorea.ge.sgp02.s12345678.cards02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnRestart(View view) {
+        Log.d(TAG, "onBtnRestart");
+    }
+    public void onBtnCard(View view) {
+        Log.d(TAG, "onBtnCard: " + view.getId());
     }
 }
