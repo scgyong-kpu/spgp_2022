a32fd0491e85405e12154c561b2577624499aeae
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-23 13:09:54 +0900

    EditText 에 TextWatcher 추가

--- projects/MoreControls01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols01/MainActivity.java
+++ projects/MoreControls01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols01/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.tukorea.ge.sgp02.s12345678.morecontrols01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -22,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edit = findViewById(R.id.nameEdit);
         output = findViewById(R.id.outputTextView);
+
+
+        edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.v(TAG, "before");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d(TAG, "textChange: " + charSequence);
+                output.setText("Text Length: " + charSequence.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.v(TAG, "after");
+            }
+        });
     }
 
     public void onBtnDoIt(View view) {
