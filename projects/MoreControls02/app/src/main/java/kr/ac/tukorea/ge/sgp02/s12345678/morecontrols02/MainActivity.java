5335abaf8095ac794f4db86a3d1e8e3a6eaf3872
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-23 12:57:04 +0900

    checkbox

--- projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/MainActivity.java
+++ projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/MainActivity.java
@@ -3,12 +3,28 @@ package kr.ac.tukorea.ge.sgp02.s12345678.morecontrols02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private CheckBox checkbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkbox = findViewById(R.id.checkbox);
+    }
+
+    public void onCheckbox(View view) {
+        CheckBox cb = (CheckBox) view;
+        Log.d(TAG, "Checkbox state: " + cb.isChecked());
+    }
+
+    public void onBtnDoIt(View view) {
+        Log.d(TAG, "onBtnDoIt, check=" + checkbox.isChecked());
     }
 }
