f61009bbd0b8e19bf8411c240979115c857c548c
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-23 13:09:54 +0900

    EditText 에 TextWatcher 추가

--- projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/MainActivity.java
+++ projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/MainActivity.java
@@ -3,6 +3,8 @@ package kr.ac.tukorea.ge.sgp02.s12345678.morecontrols02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
@@ -23,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
         checkbox = findViewById(R.id.checkbox);
         outputTextView = findViewById(R.id.outputTextView);
         nameEdit = findViewById(R.id.nameEdit);
+
+        nameEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.v(TAG, "before");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d(TAG, "textChange: " + charSequence);
+                outputTextView.setText("Text Length: " + charSequence.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.v(TAG, "after");
+            }
+        });
     }
 
     public void onCheckbox(View view) {
