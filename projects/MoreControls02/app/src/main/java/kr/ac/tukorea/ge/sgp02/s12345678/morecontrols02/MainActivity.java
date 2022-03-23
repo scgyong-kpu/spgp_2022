b7d5ae4b0c251b8cc72537a86ca1781b1cc7b7ef
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-23 13:01:49 +0900

    EditText 추가.

--- projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/MainActivity.java
+++ projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/MainActivity.java
@@ -6,17 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private CheckBox checkbox;
+    private TextView outputTextView;
+    private EditText nameEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkbox = findViewById(R.id.checkbox);
+        outputTextView = findViewById(R.id.outputTextView);
+        nameEdit = findViewById(R.id.nameEdit);
     }
 
     public void onCheckbox(View view) {
@@ -26,5 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnDoIt(View view) {
         Log.d(TAG, "onBtnDoIt, check=" + checkbox.isChecked());
+        String text = nameEdit.getText().toString();
+        outputTextView.setText(text);
     }
 }
