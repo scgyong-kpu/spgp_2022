4685c5f03ac06b117a72fec430deb5fc23e479d1
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-23 11:11:22 +0900

    MoreControls and checkbox

---
+++ projects/MoreControls01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols01/MainActivity.java
@@ -0,0 +1,29 @@
+package kr.ac.tukorea.ge.sgp02.s12345678.morecontrols01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnDoIt(View view) {
+        CheckBox cb = findViewById(R.id.checkBox);
+        Log.d(TAG, "onBtnDoIt(), Checked: " + cb.isChecked());
+    }
+
+    public void onCheckbox(View view) {
+        CheckBox cb = (CheckBox) view;
+        Log.d(TAG, "Checked: " + cb.isChecked());
+    }
+}
