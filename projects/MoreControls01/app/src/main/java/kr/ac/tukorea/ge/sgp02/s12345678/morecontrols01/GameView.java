03a6d5a394394d25f11835dffab76723ff668ab6
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-24 16:30:40 +0900

    onDraw 구현. width/height, padding 적용.

--- projects/MoreControls01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols01/GameView.java
+++ projects/MoreControls01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols01/GameView.java
@@ -1,6 +1,9 @@
 package kr.ac.tukorea.ge.sgp02.s12345678.morecontrols01;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -15,5 +18,21 @@ public class GameView extends View {
     public GameView(Context context, AttributeSet as) {
         super(context, as);
         Log.d(TAG, "GameView cons with as");
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //super.onDraw(canvas);
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
+        int right = getPaddingRight();
+        int bottom = getPaddingBottom();
+        int width = getWidth();
+        int height = getHeight();
+        Log.d(TAG, "size: " + width + "," + height + " padding: " + left + "," + top + "," + right + "," + bottom);
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#000000"));
+        float rx = width / 10, ry = height / 10;
+        canvas.drawRoundRect(left, top, width - right, height - bottom, rx, ry, paint);
     }
 }
