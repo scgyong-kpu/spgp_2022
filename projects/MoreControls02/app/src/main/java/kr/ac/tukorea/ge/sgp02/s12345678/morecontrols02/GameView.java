200b2d5d2d33c554f85eee1fdcd643968d306040
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-03-28 16:37:08 +0900

    Ball Image 그리기 추가.

--- projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/GameView.java
+++ projects/MoreControls02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/morecontrols02/GameView.java
@@ -1,32 +1,57 @@
 package kr.ac.tukorea.ge.sgp02.s12345678.morecontrols02;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private Paint paint = new Paint();
+    private Bitmap soccerBitmap;
+    private Rect soccerSrcRect = new Rect();
+    private Rect soccerDstRect = new Rect();
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initView();
+    }
+
+    private void initView() {
+        Resources res = getResources();
+        soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+        soccerSrcRect.set(0, 0, soccerBitmap.getWidth(), soccerBitmap.getHeight());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         //super.onDraw(canvas);
-        Paint paint = new Paint();
+        //Paint paint = new Paint();
         int width = getWidth();
         int height = getHeight();
-        int lp = getPaddingLeft();
-        int tp = getPaddingTop();
-        int rp = getPaddingRight();
-        int bp = getPaddingBottom();
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
 
-        int cw = width - lp - rp;
-        int ch = height - tp - bp;
+        int contentWidth = width - paddingLeft - paddingRight;
+        int contentHeight = height - paddingTop - paddingBottom;
 
-        canvas.drawRoundRect(lp, tp, lp + cw, tp + ch, 30, 40, paint);
+        int size = contentWidth < contentHeight ? contentWidth : contentHeight;
+        int ballRadius = size / 10;
+
+        int centerX = paddingLeft + contentWidth / 2;
+        int centerY = paddingTop + contentHeight / 2;
+
+        canvas.drawRoundRect(paddingLeft, paddingTop, paddingLeft + contentWidth, paddingTop + contentHeight, 30, 40, paint);
+
+        soccerDstRect.set(centerX - ballRadius, centerY - ballRadius,
+                centerX + ballRadius, centerY + ballRadius);
+        canvas.drawBitmap(soccerBitmap, soccerSrcRect, soccerDstRect, null);
     }
 }
