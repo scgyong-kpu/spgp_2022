ec7eb9358f32fe271f83033eb05d5d1943ba008b
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-04-01 15:52:46 +0900

    GameView 추가하고 축구공 그리기

---
+++ projects/SampleGame02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/samplegame02/GameView.java
@@ -0,0 +1,39 @@
+package kr.ac.tukorea.ge.sgp02.s12345678.samplegame02;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameView extends View {
+    private static final String TAG = GameView.class.getSimpleName();
+    private Bitmap soccerBitmap;
+    private Rect soccerSrcRect = new Rect();
+    private Rect soccerDstRect = new Rect();
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    private void initView() {
+        Resources res = getResources();
+        soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+
+        soccerSrcRect.set(0, 0, soccerBitmap.getWidth(), soccerBitmap.getHeight());
+        soccerDstRect.set(0, 0, 200, 200);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(soccerBitmap, soccerSrcRect, soccerDstRect, null);
+        Log.d(TAG, "onDraw()");
+    }
+}
