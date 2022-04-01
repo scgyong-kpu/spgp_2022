df74691c974f0cc591cc61ee43fb2553350b2dde
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-04-01 17:33:31 +0900

    Ball class 생성.

---
+++ projects/SampleGame01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/samplegame01/Ball.java
@@ -0,0 +1,12 @@
+package kr.ac.tukorea.ge.sgp02.s12345678.samplegame01;
+
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+
+public class Ball {
+    private int dx, dy;
+    private Rect dstRect = new Rect();
+
+    private static Bitmap bitmap;
+    private static Rect srcRect = new Rect();
+}
