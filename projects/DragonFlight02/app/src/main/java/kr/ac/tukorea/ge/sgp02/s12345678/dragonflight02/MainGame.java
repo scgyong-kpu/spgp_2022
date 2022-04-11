cae15f4bd75f9c4291fd9eedcde621ddebf64dfd
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-04-11 16:58:52 +0900

    ConcurrentModificationException 이 발생하지 않도록 View.post() 를 이용하여 나중에 추가/삭제 되도록 한다.

--- projects/DragonFlight02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight02/MainGame.java
+++ projects/DragonFlight02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight02/MainGame.java
@@ -72,10 +72,20 @@ public class MainGame {
     }
 
     public void add(GameObject gameObject) {
-        objects.add(gameObject);
+        GameView.view.post(new Runnable() {
+            @Override
+            public void run() {
+                objects.add(gameObject);
+            }
+        });
     }
 
     public void remove(GameObject gameObject) {
-        objects.remove(gameObject);
+        GameView.view.post(new Runnable() {
+            @Override
+            public void run() {
+                objects.remove(gameObject);
+            }
+        });
     }
 }
