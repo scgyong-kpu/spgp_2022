dcafd2de311493fc44388eac70e6e0153d5aaf80
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-04-13 12:00:05 +0900

    Enemy 가 생성되는 y 위치는 0 이 아니고 -size/2 여야 한다.

--- projects/DragonFlight02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight02/game/Enemy.java
+++ projects/DragonFlight02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight02/game/Enemy.java
@@ -7,8 +7,14 @@ import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight02.framework.Sprite;
 public class Enemy extends Sprite {
     protected float dy;
     public Enemy(float x, float speed) {
-        super(x, 0, R.dimen.enemy_radius, R.mipmap.f_01_01);
+//        super(x, 0, R.dimen.enemy_radius, R.mipmap.f_01_01);
+        super(x, -size/2, size, size, R.mipmap.f_01_01);
         dy = speed;
+    }
+
+    private static float size;
+    public static void setSize(float size) {
+        Enemy.size = size;
     }
 
     @Override
--- projects/DragonFlight02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight02/game/EnemyGenerator.java
+++ projects/DragonFlight02/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight02/game/EnemyGenerator.java
@@ -7,7 +7,7 @@ import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight02.framework.GameObject;
 import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight02.framework.Metrics;
 
 public class EnemyGenerator implements GameObject {
-    private static final float INITIAL_SPAWN_INTERVAL = 5.0f;
+    private static final float INITIAL_SPAWN_INTERVAL = 2.0f;
     private final float spawnInterval;
     private final float fallSpeed;
     private float elapsedTime;
@@ -15,6 +15,9 @@ public class EnemyGenerator implements GameObject {
     public EnemyGenerator() {
         this.spawnInterval = INITIAL_SPAWN_INTERVAL;
         this.fallSpeed = Metrics.size(R.dimen.enemy_initial_speed);
+
+        float enemySize = Metrics.width / 5.0f;
+        Enemy.setSize(enemySize);
     }
 
     @Override
