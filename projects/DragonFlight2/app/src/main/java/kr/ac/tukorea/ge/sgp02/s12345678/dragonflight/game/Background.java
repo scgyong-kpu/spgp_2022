28b2d73841df754b778817cf4bf0bd2e83806cec
Kiyong Kim <empty email>
2022-04-27 02:18:26 +0900

    스크롤하여 움직이도록.

--- projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/game/Background.java
+++ projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/game/Background.java
@@ -4,9 +4,22 @@ import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.framework.Metrics;
 import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.framework.Sprite;
 
 public class Background extends Sprite {
-    public Background(int bitmapResId) {
+    private final float speed;
+    private final float height;
+
+    public Background(int bitmapResId, float speed) {
         super(Metrics.width / 2, Metrics.height / 2, Metrics.width, Metrics.height, bitmapResId);
-        float height = bitmap.getHeight() * Metrics.width / bitmap.getWidth();
+        this.height = bitmap.getHeight() * Metrics.width / bitmap.getWidth();
+        setDstRect(Metrics.width, height);
+        this.speed = speed;
+    }
+
+    @Override
+    public void update() {
+        this.y += speed * MainGame.getInstance().frameTime;
+        if (this.y > Metrics.height) {
+            this.y = 0;
+        }
         setDstRect(Metrics.width, height);
     }
 }
