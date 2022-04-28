b81eda30bcd690248bb209947b3228cf0cee07fc
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-04-28 17:08:16 +0900

    MainActivity -> GameActivity 로 버튼을 사용하여 실행시킨다.

--- projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/app/MainActivity.java
+++ projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/app/MainActivity.java
@@ -2,7 +2,9 @@ package kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.R;
 import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.framework.GameView;
@@ -16,22 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onPause() {
-        GameView.view.pauseGame();
-        super.onPause();
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        GameView.view.resumeGame();
-    }
-
-    @Override
-    protected void onDestroy() {
-        GameView.view = null;
-        MainGame.clear();
-        super.onDestroy();
+    public void onBtnStart(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
