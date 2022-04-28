9ea07b32af9bf3a7659a4c7ad93de443291e2fe0
Kiyong Kim <empty email>
2022-04-28 11:39:31 +0900

    Button 만들어서 Game 에 진입

--- projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/app/MainActivity.java
+++ projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/app/MainActivity.java
@@ -2,7 +2,9 @@ package kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.R;
 import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.framework.GameView;
@@ -16,4 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onBtnStart(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
 }
