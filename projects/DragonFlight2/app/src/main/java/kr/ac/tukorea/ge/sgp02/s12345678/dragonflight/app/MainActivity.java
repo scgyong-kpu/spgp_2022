b4854c32c88ea04b34a765a387e5231d5af92923
Kiyong Kim <empty email>
2022-05-04 10:10:45 +0900

    이미지 1장으로 애니메이션.

--- projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/app/MainActivity.java
+++ projects/DragonFlight2/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/dragonflight/app/MainActivity.java
@@ -5,19 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
 import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.R;
-import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.framework.GameView;
-import kr.ac.tukorea.ge.sgp02.s12345678.dragonflight.game.MainGame;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private View tree1;
-    private View tree2;
+    private View tree;
     private ValueAnimator animator;
 
     @Override
@@ -25,8 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tree1 = findViewById(R.id.tree1);
-        tree2 = findViewById(R.id.tree2);
+        tree = findViewById(R.id.tree1);
         createAnimator();
     }
 
@@ -43,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createAnimator() {
-        animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator = ValueAnimator.ofFloat(0.0f, 0.5f);
         animator.setDuration(30000);
         animator.setRepeatCount(ValueAnimator.INFINITE);
         animator.setInterpolator(new LinearInterpolator());
@@ -52,9 +47,8 @@ public class MainActivity extends AppCompatActivity {
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 float progress = (Float)valueAnimator.getAnimatedValue();
 //                Log.d(TAG, "Progerss: " + progress);
-                float tx = -1 * tree1.getWidth() * progress;
-                tree1.setTranslationX(tx);
-                tree2.setTranslationX(tx);
+                float tx = -1 * tree.getWidth() * progress;
+                tree.setTranslationX(tx);
             }
         });
     }
