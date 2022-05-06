1fd4e40cac1e8cd6733f287ba54826def779040b
Kiyong Kim <empty email>
2022-05-06 23:16:13 +0900

    좌표 미세조정 하고 BoxCollidable 구현.

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Player.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Player.java
@@ -1,11 +1,13 @@
 package net.scgyong.and.cookierun.game;
 
 import android.graphics.Rect;
+import android.graphics.RectF;
 
 import net.scgyong.and.cookierun.R;
+import net.scgyong.and.cookierun.framework.interfaces.BoxCollidable;
 import net.scgyong.and.cookierun.framework.objects.SheetSprite;
 
-public class Player extends SheetSprite {
+public class Player extends SheetSprite implements BoxCollidable {
 
     private static final float FRAMES_PER_SECOND = 8f;
 
@@ -15,10 +17,15 @@ public class Player extends SheetSprite {
         this.y = y;
         setDstRect(w, h);
         srcRects = new Rect[] {
-                new Rect(77, 409, 197, 544),
-                new Rect(77 + 272, 409, 197 + 272, 544),
-                new Rect(77 + 2 * 272, 409, 197 + 2 * 272, 544),
-                new Rect(77 + 3 * 272, 409, 197 + 3 * 272, 544),
+                new Rect(72 + 0 * 272, 404, 72+140 + 0 * 272, 404+140),
+                new Rect(72 + 1 * 272, 404, 72+140 + 1 * 272, 404+140),
+                new Rect(72 + 2 * 272, 404, 72+140 + 2 * 272, 404+140),
+                new Rect(72 + 3 * 272, 404, 72+140 + 3 * 272, 404+140),
         };
+    }
+
+    @Override
+    public RectF getBoundingRect() {
+        return dstRect;
     }
 }
