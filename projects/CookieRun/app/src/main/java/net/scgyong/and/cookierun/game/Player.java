35fe43e0c6e397ac7f3bfd66e5cebd5788c5a003
Kiyong Kim <empty email>
2022-05-07 00:05:37 +0900

    state 별 좌표를 지정하는 더 간단한 방법.

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Player.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Player.java
@@ -8,28 +8,53 @@ import net.scgyong.and.cookierun.R;
 import net.scgyong.and.cookierun.framework.interfaces.BoxCollidable;
 import net.scgyong.and.cookierun.framework.objects.SheetSprite;
 
+import java.util.ArrayList;
+
 public class Player extends SheetSprite implements BoxCollidable {
 
     private static final float FRAMES_PER_SECOND = 8f;
     private static final String TAG = Player.class.getSimpleName();
 
+    static {
+        State.initRects();
+    }
     private enum State {
         run, jump, COUNT;
         Rect[] srcRects() {
             return rects[this.ordinal()];
         }
-        static Rect[][] rects = {
-            new Rect[] {
-                new Rect(72 + 0 * 272, 404, 72+140 + 0 * 272, 404+140),
-                new Rect(72 + 1 * 272, 404, 72+140 + 1 * 272, 404+140),
-                new Rect(72 + 2 * 272, 404, 72+140 + 2 * 272, 404+140),
-                new Rect(72 + 3 * 272, 404, 72+140 + 3 * 272, 404+140)
-            },
-            new Rect[] {
-                new Rect(72 + 7 * 272, 132, 72+140 + 7 * 272, 132+140),
-                new Rect(72 + 8 * 272, 132, 72+140 + 8 * 272, 132+140),
-            },
-        };
+        static Rect[][] rects;
+//        = {
+//            new Rect[] {
+//                new Rect(72 + 0 * 272, 404, 72+140 + 0 * 272, 404+140),
+//                new Rect(72 + 1 * 272, 404, 72+140 + 1 * 272, 404+140),
+//                new Rect(72 + 2 * 272, 404, 72+140 + 2 * 272, 404+140),
+//                new Rect(72 + 3 * 272, 404, 72+140 + 3 * 272, 404+140)
+//            },
+//            new Rect[] {
+//                new Rect(72 + 7 * 272, 132, 72+140 + 7 * 272, 132+140),
+//                new Rect(72 + 8 * 272, 132, 72+140 + 8 * 272, 132+140),
+//            },
+//        };
+        static void initRects() {
+            int[][] indices = {
+                    new int[] { 100, 101, 102, 103 }, // run
+                    new int[] { 7, 8 }, // jump
+            };
+            ArrayList<Rect[]> rectsList = new ArrayList<>();
+            for (int[] ints : indices) {
+                Rect[] rects = new Rect[ints.length];
+                for (int i = 0; i < ints.length; i++) {
+                    int idx = ints[i];
+                    int l = 72 + (idx % 100) * 272;
+                    int t = 132 + (idx / 100) * 272;
+                    Rect rect = new Rect(l, t, l + 140, t + 140);
+                    rects[i] = rect;
+                }
+                rectsList.add(rects);
+            }
+            rects = rectsList.toArray(new Rect[rectsList.size()][]);
+        }
     }
     private State state = State.run;
 
