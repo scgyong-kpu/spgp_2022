51b24be63b0d7a98ae124be928078983c3191ca3
Kiyong Kim <empty email>
2022-05-06 23:35:47 +0900

    Jump State 를 만들고 jump/run 일 때 각기 다른 사각형으로부터 애니메이션하도록

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Player.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Player.java
@@ -13,17 +13,32 @@ public class Player extends SheetSprite implements BoxCollidable {
     private static final float FRAMES_PER_SECOND = 8f;
     private static final String TAG = Player.class.getSimpleName();
 
+    private enum State {
+        run, jump, COUNT;
+        Rect[] srcRects() {
+            return rects[this.ordinal()];
+        }
+        static Rect[][] rects = {
+            new Rect[] {
+                new Rect(72 + 0 * 272, 404, 72+140 + 0 * 272, 404+140),
+                new Rect(72 + 1 * 272, 404, 72+140 + 1 * 272, 404+140),
+                new Rect(72 + 2 * 272, 404, 72+140 + 2 * 272, 404+140),
+                new Rect(72 + 3 * 272, 404, 72+140 + 3 * 272, 404+140)
+            },
+            new Rect[] {
+                new Rect(72 + 7 * 272, 132, 72+140 + 7 * 272, 132+140),
+                new Rect(72 + 8 * 272, 132, 72+140 + 8 * 272, 132+140),
+            },
+        };
+    }
+    private State state = State.run;
+
     public Player(float x, float y, float w, float h) {
         super(R.mipmap.cookie, FRAMES_PER_SECOND);
         this.x = x;
         this.y = y;
         setDstRect(w, h);
-        srcRects = new Rect[] {
-                new Rect(72 + 0 * 272, 404, 72+140 + 0 * 272, 404+140),
-                new Rect(72 + 1 * 272, 404, 72+140 + 1 * 272, 404+140),
-                new Rect(72 + 2 * 272, 404, 72+140 + 2 * 272, 404+140),
-                new Rect(72 + 3 * 272, 404, 72+140 + 3 * 272, 404+140),
-        };
+        setState(State.run);
     }
 
     @Override
@@ -33,5 +48,15 @@ public class Player extends SheetSprite implements BoxCollidable {
 
     public void jump() {
         Log.d(TAG, "Jump");
+        if (state == State.run) {
+            setState(State.jump);
+        } else {
+            setState(State.run);
+        }
+    }
+
+    private void setState(State state) {
+        this.state = state;
+        srcRects = state.srcRects();
     }
 }
