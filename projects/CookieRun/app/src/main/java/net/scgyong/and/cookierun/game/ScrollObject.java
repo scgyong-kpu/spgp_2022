a0ca91c03bf6e6ecbac4f9e8a746a135c51411a5
Kiyong Kim <empty email>
2022-05-06 13:17:37 +0900

    움직이게 하고 화면을 벗어나면 삭제. GameActivity 를 재시작하면 재활용되는 것을 확인할 수 있다.

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/ScrollObject.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/ScrollObject.java
@@ -1,13 +1,29 @@
 package net.scgyong.and.cookierun.game;
 
 import android.graphics.RectF;
+import android.util.Log;
 
+import net.scgyong.and.cookierun.framework.game.BaseGame;
 import net.scgyong.and.cookierun.framework.interfaces.BoxCollidable;
 import net.scgyong.and.cookierun.framework.interfaces.Recyclable;
 import net.scgyong.and.cookierun.framework.objects.Sprite;
 
 public class ScrollObject extends Sprite implements Recyclable, BoxCollidable {
+    private static final String TAG = ScrollObject.class.getSimpleName();
+
     protected ScrollObject() {
+        Log.d(TAG, "New:" + this);
+    }
+
+    @Override
+    public void update(float frameTime) {
+        float speed = -200;
+        float dx = speed * frameTime;
+        dstRect.offset(dx, 0);
+        if (dstRect.right < 0) {
+            Log.d(TAG, "Removing:" + this);
+            BaseGame.getInstance().remove(this);
+        }
     }
 
     @Override
