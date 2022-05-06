5b8a890412d053a42bc09f678c52a064d090b9e4
Kiyong Kim <empty email>
2022-05-06 15:46:35 +0900

    디버그용 코드 삭제.

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/MapLoader.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/MapLoader.java
@@ -17,7 +17,7 @@ public class MapLoader implements GameObject {
     private final float unit;
     public float speed = -200;
     public float scroll, itemLeft, platformLeft;
-    private Paint itemPaint, platformPaint;
+//    private Paint itemPaint, platformPaint;
 
     private MapLoader() {
         random = new Random();
@@ -32,13 +32,13 @@ public class MapLoader implements GameObject {
     public void init() {
         scroll = 0;
         itemLeft = platformLeft = 0;
-        itemPaint = new Paint();
-        itemPaint.setColor(Color.RED);
-        itemPaint.setStrokeWidth(3);
-
-        platformPaint = new Paint();
-        platformPaint.setColor(Color.BLUE);
-        platformPaint.setStrokeWidth(5);
+//        itemPaint = new Paint();
+//        itemPaint.setColor(Color.RED);
+//        itemPaint.setStrokeWidth(3);
+//
+//        platformPaint = new Paint();
+//        platformPaint.setColor(Color.BLUE);
+//        platformPaint.setStrokeWidth(5);
     }
     @Override
     public void update(float frameTime) {
@@ -48,35 +48,35 @@ public class MapLoader implements GameObject {
 
         while (true) {
             float left = scroll + itemLeft;
-            if (left > Metrics.width/2 + unit) {
+            if (left > Metrics.width + unit) {
                 break;
             }
             int index = random.nextInt(JellyItem.JELLY_COUNT);
             float unitTop = random.nextInt(8);
             JellyItem item = JellyItem.get(index, (left / unit), unitTop);
             game.add(MainGame.Layer.item.ordinal(), item);
-            Log.d(TAG, "itemLeft=" + itemLeft + " left=" + left + " w=" + item.dstWidth());
+//            Log.d(TAG, "itemLeft=" + itemLeft + " left=" + left + " w=" + item.dstWidth());
             itemLeft += item.dstWidth();
-            break;
+//            break;
         }
 
         while (true) {
             float left = scroll + platformLeft;
-            if (left > Metrics.width/2 + unit) {
+            if (left > Metrics.width + unit) {
                 break;
             }
             Platform.Type type = Platform.Type.random(random);
             Platform platform = Platform.get(type, (left / unit), 8);
             game.add(MainGame.Layer.platform.ordinal(), platform);
-            Log.d(TAG, "platformLeft=" + platformLeft + " left=" + left + " w=" + platform.dstWidth());
+//            Log.d(TAG, "platformLeft=" + platformLeft + " left=" + left + " w=" + platform.dstWidth());
             platformLeft += platform.dstWidth();
-            break;
+//            break;
         }
     }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawLine(scroll + platformLeft, 0, scroll + platformLeft, Metrics.height, platformPaint);
-        canvas.drawLine(scroll + itemLeft, 0, scroll + itemLeft, Metrics.height, itemPaint);
+//        canvas.drawLine(scroll + platformLeft, 0, scroll + platformLeft, Metrics.height, platformPaint);
+//        canvas.drawLine(scroll + itemLeft, 0, scroll + itemLeft, Metrics.height, itemPaint);
     }
 }
