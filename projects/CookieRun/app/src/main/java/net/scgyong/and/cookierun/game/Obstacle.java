b4fc7ed502b886abaaef37e2e6543ab0bf8815b2
Kiyong Kim <empty email>
2022-05-17 17:54:15 +0900

    Animation 이 진행될 수 있도록.

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Obstacle.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/Obstacle.java
@@ -6,6 +6,7 @@ import net.scgyong.and.cookierun.framework.res.BitmapPool;
 
 public class Obstacle extends MapSprite {
     private Modifier modifier;
+    private long createdOn;
 
     public static Obstacle get(int index, float unitLeft, float unitTop) {
         Obstacle obs = (Obstacle) RecycleBin.get(Obstacle.class);
@@ -16,8 +17,8 @@ public class Obstacle extends MapSprite {
         return obs;
     }
     private static class Modifier {
-        private final float height;
-        private int mipmapResId;
+        protected final float height;
+        protected int mipmapResId;
         public Modifier(float heightUnit) {
             this.height = heightUnit;
         }
@@ -25,7 +26,7 @@ public class Obstacle extends MapSprite {
             this.height = heightUnit;
             this.mipmapResId = mipmapResId;
         }
-        public void update(float frameTime) {
+        public void update(Obstacle obstacle, float frameTime) {
         }
         public void init(Obstacle obstacle, float unitLeft, float unitTop) {
             unitTop -= this.height - 1;
@@ -46,6 +47,20 @@ public class Obstacle extends MapSprite {
         public void init(Obstacle obstacle, float unitLeft, float unitTop) {
             super.init(obstacle, unitLeft, unitTop);
             obstacle.bitmap = BitmapPool.get(mipmapResIds[0]);
+            obstacle.createdOn = System.currentTimeMillis();
+        }
+
+        @Override
+        public void update(Obstacle obstacle, float frameTime) {
+            float elapsed = (System.currentTimeMillis() - obstacle.createdOn) / 1000.f;
+            final float start = 2.0f;
+            if (elapsed > start) {
+                final float fps = 8.0f;
+                int index = Math.round((elapsed - start) * fps);
+                if (index < mipmapResIds.length) {
+                    obstacle.bitmap = BitmapPool.get(mipmapResIds[index]);
+                }
+            }
         }
     }
     private static class MoveModifier extends Modifier {
@@ -75,5 +90,11 @@ public class Obstacle extends MapSprite {
     private void init(int index, float unitLeft, float unitTop) {
         modifier = MODIFIERS[index];
         modifier.init(this, unitLeft, unitTop);
+    }
+
+    @Override
+    public void update(float frameTime) {
+        super.update(frameTime);
+        modifier.update(this, frameTime);
     }
 }
