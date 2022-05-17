e2dc5b2edae48bbecf620c245b1dade417a7578d
Kiyong Kim <empty email>
2022-05-17 16:09:23 +0900

    쿠키가 원래 크기대로 나오려면 그리는 영역을 키워야 한다.

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/MainGame.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/MainGame.java
@@ -42,7 +42,7 @@ public class MainGame extends BaseGame {
 //                R.mipmap.cookie);
         player = new Player(
                 size(2), size(2),
-                size(2), size(2)
+                size(3.85f), size(3.85f)
         );
         add(Layer.player.ordinal(), player);
         add(Layer.bg.ordinal(), new HorzScrollBackground(R.mipmap.cookie_run_bg_1, Metrics.size(R.dimen.bg_scroll_1)));
