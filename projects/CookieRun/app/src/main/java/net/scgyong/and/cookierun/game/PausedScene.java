aaed28db63c017438d55652b61a8ecaf4ac7f014
Kiyong Kim <empty email>
2022-05-17 19:30:52 +0900

    버튼 추가.

--- projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/PausedScene.java
+++ projects/CookieRun/app/src/main/java/net/scgyong/and/cookierun/game/PausedScene.java
@@ -2,6 +2,7 @@ package net.scgyong.and.cookierun.game;
 
 import net.scgyong.and.cookierun.R;
 import net.scgyong.and.cookierun.framework.game.Scene;
+import net.scgyong.and.cookierun.framework.objects.Button;
 import net.scgyong.and.cookierun.framework.objects.Sprite;
 import net.scgyong.and.cookierun.framework.res.Metrics;
 
@@ -27,5 +28,30 @@ public class PausedScene extends Scene {
                 Metrics.width / 2, Metrics.height / 4,
                 Metrics.width / 3, Metrics.width / 3 * 230 / 440,
                 R.mipmap.game_paused));
+
+        float btn_width = Metrics.width / 4;
+        float btn_height = btn_width * 192 / 512;
+        float btn_x = Metrics.width / 2;
+        float btn_y = Metrics.height / 2 + btn_height / 2;
+
+        add(Layer.touchUi.ordinal(), new Button(
+                btn_x, btn_y, btn_width, btn_height,
+                R.mipmap.btn_resume_n, R.mipmap.btn_resume_p, new Button.Callback()
+        {
+            @Override
+            public boolean onTouch(Button.Action action) {
+                return false;
+            }
+        }));
+        btn_y += btn_height;
+        add(Layer.touchUi.ordinal(), new Button(
+                btn_x, btn_y, btn_width, btn_height,
+                R.mipmap.btn_exit_n, R.mipmap.btn_exit_p, new Button.Callback()
+        {
+            @Override
+            public boolean onTouch(Button.Action action) {
+                return false;
+            }
+        }));
     }
 }
