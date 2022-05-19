7ff5fc8c838fdc396336b553d487a97f9c0ad75f
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-05-19 15:54:37 +0900

    Touch 로 라인 그리기.

--- projects/SmoothingPath01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/smoothingpath01/PathView.java
+++ projects/SmoothingPath01/app/src/main/java/kr/ac/tukorea/ge/sgp02/s12345678/smoothingpath01/PathView.java
@@ -5,16 +5,23 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
 
 /**
  * TODO: document your custom view class.
  */
 public class PathView extends View {
     private int mExampleColor = Color.RED; // TODO: use a default from R.color...
+
+    private Paint paint;
 
     public PathView(Context context) {
         super(context);
@@ -42,23 +49,28 @@ public class PathView extends View {
 
 
         a.recycle();
+
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(2.0f);
+        paint.setColor(mExampleColor);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
+        int ptCount = points.size();
+        if (ptCount < 2) {
+            return;
+        }
+        Path path = new Path();
+        path.moveTo(points.get(0).x, points.get(0).y);
+        for (int i = 1; i < ptCount; i++) {
+            PointF pt = points.get(i);
+            path.lineTo(pt.x, pt.y);
+        }
+        canvas.drawPath(path, paint);
 
     }
 
@@ -81,5 +93,18 @@ public class PathView extends View {
     public void setExampleColor(int exampleColor) {
         mExampleColor = exampleColor;
         //invalidateTextPaintAndMeasurements();
+    }
+
+    ArrayList<PointF> points = new ArrayList<>();
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            PointF point = new PointF();
+            point.x = event.getX();
+            point.y = event.getY();
+            points.add(point);
+            invalidate();
+        }
+        return super.onTouchEvent(event);
     }
 }
