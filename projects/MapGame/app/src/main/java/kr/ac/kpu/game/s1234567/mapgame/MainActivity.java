adf2113f8a83d1aa4d093a0a60992e0cfdc75006
Kiyong Kim <scgyong@gmail.com>
2022-05-24 14:14:34 +0900

    MapGame skeleton

---
+++ projects/MapGame/app/src/main/java/kr/ac/kpu/game/s1234567/mapgame/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.kpu.game.s1234567.mapgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
