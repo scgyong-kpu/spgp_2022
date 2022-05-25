0f2df8631df1ff3071f0d56760d04e0f1aa4f50e
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-05-25 16:37:55 +0900

    Defense Game 준비
    (cherry picked from commit 8a035346386906690b2560d5386c127f09f8fecd)

--- projects/MapGame/app/src/main/java/kr/ac/kpu/game/s1234567/mapgame/scenes/TiledSprite.java
+++ projects/MapGame/app/src/main/java/kr/ac/kpu/game/s1234567/mapgame/scenes/TiledSprite.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import kr.ac.kpu.game.framework.map.MapLoader;
 import kr.ac.kpu.game.framework.map.TiledMap;
 import kr.ac.kpu.game.framework.objects.Sprite;
+import kr.ac.kpu.game.framework.res.Metrics;
 import kr.ac.kpu.game.framework.view.GameView;
 
 public class TiledSprite extends Sprite {
@@ -12,14 +13,7 @@ public class TiledSprite extends Sprite {
 
     public TiledSprite() {
         this.map = new MapLoader(GameView.view.getContext()).loadAsset("map", "desert.tmj");
-        map.setDstTileSize(100);
-    }
-
-    @Override
-    public void update(float frameTime) {
-        int speed = 300;
-        map.x += speed * frameTime;
-        map.y += speed * frameTime;
+        map.setDstTileSize(Metrics.height / map.height);
     }
 
     @Override
