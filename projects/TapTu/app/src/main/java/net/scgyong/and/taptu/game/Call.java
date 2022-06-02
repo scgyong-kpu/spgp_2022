751a8ff706bb287b833c64ad3f08011be6224f31
Kiyong Kim <scgyong@tukorea.ac.kr>
2022-06-03 03:45:40 +0900

    Call animation

--- projects/TapTu/app/src/main/java/net/scgyong/and/taptu/game/Call.java
+++ projects/TapTu/app/src/main/java/net/scgyong/and/taptu/game/Call.java
@@ -13,12 +13,15 @@ public class Call extends Sprite {
     private Rect srcRect = new Rect();
     private int height;
     private long setOn;
+    private float originalX, originalY;
     public Call() {
         super(Metrics.width / 2, Metrics.height / 3,
                 Metrics.width / 3, Metrics.width / 15,
                 R.mipmap.calls);
         height = bitmap.getHeight() / 5;
         srcRect.set(0, 0, bitmap.getWidth(), height);
+        originalX = dstRect.left;
+        originalY = dstRect.top;
     }
     public enum Type {
         perfect, great, good, bad, miss
@@ -33,7 +36,10 @@ public class Call extends Sprite {
     @Override
     public void draw(Canvas canvas) {
         int elapsed = (int) (System.currentTimeMillis() - setOn);
-        if (elapsed < 500) {
+        if (elapsed < 1000) {
+            if (elapsed > 500) elapsed = 500;
+            float y = originalY - elapsed * dstRect.height() / 1000.0f;
+            dstRect.offsetTo(originalX, y);
             canvas.drawBitmap(bitmap, srcRect, dstRect, null);
         }
     }
